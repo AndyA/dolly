78dc61373f7b9df24a6129ddb5a2d03d911d831f
Andy Armstrong <andy@hexten.net>
2026-03-13 15:45:27 +0000

    Kinda working

--- bin/ble-server.py
+++ bin/ble-server.py
@@ -1,19 +1,87 @@
 import asyncio
+import locale
+import logging
+import math
+import os
+import sys
+import time
 
+import gphoto2 as gp
 from pb_ble.vhub import get_virtual_ble
+
+FRAMES = "tmp/frames"
 
 
 async def main():
+    last_image_num: int = -1
+    os.makedirs(FRAMES, exist_ok=True)
+    locale.setlocale(locale.LC_ALL, "")
+    logging.basicConfig(
+        format="%(levelname)s: %(name)s: %(message)s", level=logging.WARNING
+    )
+
+    camera = gp.Camera()  # ty:ignore[unresolved-attribute]
+    camera.init()
+
     async with await get_virtual_ble(
         broadcast_channel=101,
         observe_channels=[100],
+        scanning_mode="active",
     ) as vble:
         print("Listening...")
         while True:
-            if msg := vble.observe():
-                print("Received message:", msg)
-            await asyncio.sleep(10)
+            # print("Tick!")
+            img_num = vble.observe(channel=100)
+            if img_num is not None and img_num != last_image_num:
+                file_path = camera.capture(gp.GP_CAPTURE_IMAGE)  # ty:ignore[unresolved-attribute]
+                target = os.path.join(FRAMES, f"frame{img_num:06d}.jpg")
+                print(f"frame: {target}")
+                img_file = camera.file_get(
+                    file_path.folder,
+                    file_path.name,
+                    gp.GP_FILE_TYPE_NORMAL,  # ty:ignore[unresolved-attribute]
+                )
+                img_file.save(target)
+                last_image_num = img_num
+
+            await asyncio.sleep(0.05)
 
 
 loop = asyncio.new_event_loop()
 loop.run_until_complete(main())
+
+
+RATE = 2
+
+
+def main():
+    os.makedirs(FRAMES, exist_ok=True)
+    locale.setlocale(locale.LC_ALL, "")
+    logging.basicConfig(
+        format="%(levelname)s: %(name)s: %(message)s", level=logging.WARNING
+    )
+    # callback_obj = gp.check_result(gp.use_python_logging())
+    camera = gp.Camera()
+    camera.init()
+    next_wake = math.ceil(time.time() / RATE) * RATE
+    while True:
+        sleep_time = next_wake - time.time()
+        if sleep_time > 0:
+            print("Sleeping for", sleep_time)
+            time.sleep(sleep_time)
+        print("Capturing image")
+        file_path = camera.capture(gp.GP_CAPTURE_IMAGE)
+        print("Camera file path: {0}/{1}".format(file_path.folder, file_path.name))
+        target = os.path.join(FRAMES, file_path.name)
+        print("Copying image to", target)
+        img_file = camera.file_get(
+            file_path.folder, file_path.name, gp.GP_FILE_TYPE_NORMAL
+        )
+        img_file.save(target)
+        next_wake += RATE
+    camera.exit()
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
--- bin/motion.py
+++ bin/motion.py
@@ -4,13 +4,16 @@ from pybricks.parameters import Port
 from pybricks.pupdevices import Motor
 from pybricks.tools import run_task
 
-hub = InventorHub()
+hub = InventorHub(
+    broadcast_channel=100,
+    observe_channels=[101],
+)
 # hub.display.text("Hello, World")
 
 
-tilt_motor = Motor(Port.F)
-pan_motor = Motor(Port.B)
-drive_motor = Motor(Port.D)
+tilt_motor = Motor(Port.F, gears=[[1, 40], [8, 28]])
+pan_motor = Motor(Port.B, gears=[[1, 40], [24, 24], [12, 60]])
+drive_motor = Motor(Port.D, gears=[[8, 40]])
 
 print("Waiting for XBox Controller...")
 
@@ -30,17 +33,21 @@ controller = XboxController()
 #     v_motor.track_target(col.v)
 
 
-def clamp(value, min_value, max_value):
-    return max(min(value, max_value), min_value)
+# def clamp(value, min_value, max_value):
+#     return max(min(value, max_value), min_value)
 
 
-# SPEED = 500
 async def main():
+    print("Starting")
+    sent = False
+    seq = 0
     while True:
         # await multitask(
         #     track_colour(),
         #     track_distance(),
         # )
+        left, right = controller.triggers()
+
         x, y = controller.joystick_left()
         _, drive = controller.joystick_right()
         # left = clamp(y + x, -100, 100)
@@ -49,6 +56,15 @@ async def main():
         pan_motor.dc(-x)
         tilt_motor.dc(y)
         drive_motor.dc(drive)
+
+        if right > 10:
+            if not sent:
+                await hub.ble.broadcast(seq)
+                print("snap:", seq)
+                seq += 1
+                sent = True
+        else:
+            sent = False
 
 
 run_task(main())
